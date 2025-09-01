73d2d3a95c9cccb313e65ae109d86420a2320e9d
Prince Gupta <guptaprince202004@gmail.com>
2025-09-01 22:01:53 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-    //    const heading = React.createElement(
-    //     "h1",
-    //     {id:"heading ",key:"abc"},
-    //     "Hello WOrld From React!"
-    // );
-
-    //     const root = ReactDOM.createRoot(document.getElementById("root"));
-
-    //     root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
-    const parent = React.createElement("div", { id: "parent" }, [
-        React.createElement("div", { id: "child" }, [
-            React.createElement("h1", { id: "heading" }, "I am H1 Tag"),
-            React.createElement("h2", {}, " I am h2 Tag"),
-        ]),
-    ]);
-    console.log(parent);
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+const Title =()=>(
+    <h1 className="head" tabIndex="5">
+    Namaste React using JSX🚀
+    </h1>
+ )
+
+const HeadingComponent =()=>(
+    <div id="container">
+          <Title />     
+     <h1 className="heading">Namste React Functional component </h1>
+     </div>
+);
+        
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
+
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Namaste React 🚀
+
+
+# Parcel
+- Dev Build 
+- Local Server
+- HMR= Hot  Module Replacement
+- File watching Algorithm - Written in c++
+- Caching - Faster Build
+- Image Optimization
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Error Handling
+- HTTPs
